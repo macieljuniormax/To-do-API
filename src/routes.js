2c64bb739561a3e493e28a6812582baf001ec088
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-07 22:10:33 -0300

    feat: :sparkles: add a condition to check if the task id exists in the database before trying to delete it.

--- src/routes.js
+++ src/routes.js
@@ -88,6 +88,12 @@ export const routes = [
     handler: (request, response) => {
       const { id } = request.params;
 
+      const [task] = database.select("tasks", { id });
+
+      if (!task) {
+        return response.writeHead(404).end();
+      }
+
       database.delete("tasks", id);
       return response.writeHead(204).end();
     },
