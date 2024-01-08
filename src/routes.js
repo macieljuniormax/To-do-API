8384faa544c9fe87af35256e6d0eda252d62d494
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-07 21:32:42 -0300

    feat: :sparkles: create methods POST e GET

---
+++ src/routes.js
@@ -0,0 +1,47 @@
+import { randomUUID } from "node:crypto";
+import { buildRoutePath } from "./utils/build-route-path.js";
+import { Database } from "./database.js";
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: "POST",
+    path: buildRoutePath("/tasks"),
+    handler: (request, response) => {
+      const { title, description } = request.body;
+
+      const task = {
+        id: randomUUID(),
+        title: title,
+        description: description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+
+      database.insert("tasks", task);
+
+      return response.writeHead(201).end();
+    },
+  },
+
+  {
+    method: "GET",
+    path: buildRoutePath("/tasks"),
+    handler: (request, response) => {
+      const { search } = request.query;
+
+      const tasks = database.select(
+        "tasks",
+        search
+          ? {
+              id: search,
+            }
+          : null
+      );
+
+      return response.end(JSON.stringify(tasks));
+    },
+  },
+];
